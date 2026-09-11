9ab8da6fdbf56df4913d0dfe3326039a3465317a
Erikbmhub <e.buitragom@hotmail.com>
2026-09-11 18:57:36 +0200

    EJ 1 al 9

---
+++ index.js
@@ -0,0 +1,27 @@
+//Variables
+
+let variableSinValor = ''
+let booleano1 = true
+let booleano2 = false
+const PI = 3.14
+const TAU = 2 * PI
+
+//Booleanos
+
+let booleanoAnd = booleano1 && booleano2    
+let booleanoNot = !booleano1    
+let booleanoMix0 = (booleano1 || booleano2) && (booleano1  || (booleano1 && !booleano2))
+
+//Operadores
+
+let incrementarDesp = 2
+let postIncremento = ++incrementarDesp
+resultadoDesp = postIncremento 
+console.log(resultadoDesp)
+
+//Bucles
+const contarHasta10_2 = 0;
+for (let i = 0; i < 10; i++) { 
+    contarHasta10_2++
+}
+console.log(contarHasta10_2)
