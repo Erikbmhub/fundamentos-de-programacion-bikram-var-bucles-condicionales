3b2ce58535db83919ecc9a845a761e4f65797dd4
Erikbmhub <e.buitragom@hotmail.com>
2026-09-23 22:44:55 +0200

    Terinado y revisado

--- index.js
+++ index.js
@@ -1,49 +1,58 @@
 //Variables
 //Prueba
-let variableSinValor = ''
+//1.
+let variableSinValor
+//2.
 let booleano1 = true
 let booleano2 = false
+//3.
 const PI = 3.14
+//4.    
 const TAU = 2 * PI
 
 //Booleanos
-
-let booleanoAnd = booleano1 && booleano2    
-let booleanoNot = !booleano1    
+//5.
+let booleanoAnd = booleano1 && booleano2  
+//6.  
+let booleanoNot = !booleano1;
+//7.
 let booleanoMix0 = (booleano1 || booleano2) && (booleano1  || (booleano1 && !booleano2))
 
 //Operadores
-
-let incrementarDesp = 2
-let postIncremento = ++incrementarDesp
+//8.
+let incrementarDesp = 2;
+let postIncremento = incrementarDesp++
 resultadoDesp = postIncremento 
 console.log(resultadoDesp)
 
-//Bucles
+//9.
+let incrementarAntes = 2;
+resultadoAntes = ++incrementarAntes;
+console.log(resultadoAntes)
 
-const contarHasta10_2 = 0;
+//Bucles
+//10.
+let contarHasta10_2 = 0;
 for (let i = 0; i < 10; i++) { 
     contarHasta10_2++
 }
 console.log(contarHasta10_2);
 
+//11.
 let postI = 0;
 let postJ = 0;
-
 for (let i = 0; i < 11; i++) {
     postI += postJ++;
 }
-
 console.log(postI);
 
+//12.
 let sumaPares = 0;
-
 for (let i = 0; i < 10; i++) {
     if (i % 2 === 0) {
         sumaPares += i;
     }
 }
-
 console.log(sumaPares)
 
 //Variables
@@ -73,7 +82,7 @@ let valorResta = MiNumeroFav - variableValorNumerico;
 //22.
 let valorMultiplicacion = MiNumeroFav * variableValorNumerico;
 //23.
-let valorDivision = MiNumeroFav / variableValorNumerico;
+let valorDivision = MiNumeroFav / 3;
 
 //Bucles
 //24.
